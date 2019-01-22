45881050af90356a6abe427cf330960ebffe85c4
unknown <1023826776@qq.com>
2019-01-22 22:33:59 +0800

    四舍五入

---
+++ 洛谷/5津津的储蓄计划/5津津的储蓄计划/津津的储蓄计划.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning(disable:4996)
+int main()
+{
+	int expend[12];
+	int save[12];
+	int i, j;
+
+	for (i = 0; i < 12; i++)
+	{
+		save[i] = 0;
+	}
+	for (i = 0; i < 12; i++)
+	{
+		scanf("%d", &expend[i]);
+	}
+    
+
+
+}
