41faf776365401b650442e1e68957adf1814495f
NietzscheMYB <1023826776@qq.com>
2019-02-06 15:08:15 +0800

    津津的存储计划

--- 洛谷/5津津的储蓄计划/5津津的储蓄计划/津津的储蓄计划.c
+++ 洛谷/5津津的储蓄计划/5津津的储蓄计划/津津的储蓄计划.c
@@ -3,19 +3,32 @@
 #pragma warning(disable:4996)
 int main()
 {
-	int expend[12];
-	int save[12];
-	int i, j;
-
+	int arr[12] = { 0 };
+	int i = 0,pre=0;
+	double sum=0;
 	for (i = 0; i < 12; i++)
 	{
-		save[i] = 0;
+		scanf("%d", &arr[i]);
 	}
 	for (i = 0; i < 12; i++)
 	{
-		scanf("%d", &expend[i]);
+		if (arr[i]<=(pre+300))
+		{
+			pre = 300 + pre - arr[i];
+			if (pre >= 100)
+			{
+				sum = sum + pre / 100 * 100;
+				pre = pre%100;
+			}
+			continue;
+		}
+		if (arr[i] > (pre + 300))
+		{
+			printf("-%d\n", i+1);
+			return 0;
+		}
 	}
-    
 
-
+	printf("%d\n",(int)(sum*1.2+pre));
+	return 0;
 }
