14dba443aceececf42149896f21efdd42f00e22f
NietzscheMYB <1023826776@qq.com>
2019-03-01 23:56:03 +0800

    练习1

---
+++ c++/练习1/ConsoleApplication1/ConsoleApplication1/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	printf("hello world\n");
+	int i = 123;
+	cout << "hello world"<< endl;
+	cout << " " << i << endl;
+	return 0;
+}
