0dba051cf650f3a4a63c7f137556a7dd7ecde55f
NietzscheMYB <1023826776@qq.com>
2019-12-26 21:28:19 +0800

    ConcurrentMemory

---
+++ c++/ConcurrentMemoryPool2/ConcurrentMemoryPool/CentralCache.cpp
@@ -0,0 +1,22 @@
+#include"CentralCache.h"
+
+
+CentralCache CentralCache::_inst;
+
+
+//打桩
+// 从中心缓存获取一定数量的对象给thread cache
+size_t CentralCache::FetchRangeObj(void*& start, void*& end, size_t n, size_t bytes)
+{
+	start = malloc(bytes*n);
+	end = (char*)start + bytes*(n-1);
+	void* cur = start;
+	while (cur <= end)
+	{
+		void* next = (char*)cur + bytes;
+		NEXT_OBJ(cur) = next;
+		cur = next;
+	}
+	NEXT_OBJ(end) = nullptr;
+	return n;
+}
